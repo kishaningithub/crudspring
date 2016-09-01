7b7a08f8ed9ac768a8f812665e19411d4ba598e4
Kishan B <bkishan@thoughtworks.com>
2016-09-01 18:44:01 +0530

    Added versioning

--- src/main/java/com/kishan/config/JerseyConfig.java
+++ src/main/java/com/kishan/config/JerseyConfig.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 import javax.ws.rs.ApplicationPath;
 
 @Component
-@ApplicationPath("/api")
+@ApplicationPath("/comments/v1")
 public class JerseyConfig extends ResourceConfig {
     public JerseyConfig() {
         registerEndpoints();
